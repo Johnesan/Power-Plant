09119cceec46c0b08458b2b3a1c83f963e382d35
fortune chibuike <chibuikefortune@hotmail.com>
2017-05-23 10:01:10 +0100

    View Models

---
+++ Power Plant/Power_Plant/ViewModels/HomeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Power_Plant.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Power_Plant.ViewModels
+{
+    class HomeViewModel : BaseViewModel
+    {
+        private INavigation Navigation;
+        public HomeViewModel(INavigation _Navigation)
+        {
+            PlantACommand = new Command(PlantAPage);
+            PlantBCommand = new Command(PlantBPage);
+
+            Navigation = _Navigation;
+        }
+        public ICommand PlantACommand { private get; set; }
+        public ICommand PlantBCommand { private get; set; }
+
+        public void PlantAPage()
+        {
+            Navigation.PushAsync(new PlantA());
+        }
+        public void PlantBPage()
+        {
+            Navigation.PushAsync(new PlantB());
+        }
+
+    }
+}
---
+++ Power Plant/Power_Plant/Views/PlantB.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Power_Plant.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PlantB : ContentPage
+    {
+        public PlantB()
+        {
+            InitializeComponent();
+        }
+    }
+}
